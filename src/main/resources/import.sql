c808198de998c1bc537d685700622b8e0ddb66f6
Daniel Ventura <dsventura@gmail.com>
2020-08-08 22:46:31 -0300

    Ajustes de configuração para portifólio AWS

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,14 @@ INSERT INTO operadora (categoria, nome, preco) VALUES ('movel', 'Vivo', '35');
 INSERT INTO operadora (categoria, nome, preco) VALUES ('movel', 'Nextel', '20');
 INSERT INTO operadora (categoria, nome, preco) VALUES ('movel', 'Oi', '70');
 INSERT INTO operadora (categoria, nome, preco) VALUES ('fixo', 'Oi', '100');
+
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('1980-02-01 00:00:00.000000', 'Daniel Ventura', '9874-6985', 5);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('1984-05-11 00:00:00.000000', 'Roberto Silveira', '9521-9853', 3);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('1978-01-22 00:00:00.000000', 'Manuel Afonso', '9657-3654', 7);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('1979-08-12 00:00:00.000000', 'Mariana Fonseca', '1452-9541', 5);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('2010-12-11 00:00:00.000000', 'Maria Fontana', '7896-3685', 6);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('2011-11-18 00:00:00.000000', 'Guilherme Silva', '8521-9563', 1);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('2014-01-15 00:00:00.000000', 'Paulo José', '3652-7825', 2);
+INSERT INTO public.contato (data, nome, telefone, fk_cod_operadora) VALUES ('2016-03-16 00:00:00.000000', 'Leonardo Souza', '1469-3621', 4);
+
+
